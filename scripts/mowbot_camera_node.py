eb2bc7229ff0de4d74c2d6b5f145d3a67d613e0a
Shaun Bowman <shaun_bowman@hotmail.com>
2018-08-14 22:29:07 -0400

    integrating mowbot_camera_node into ros node

--- scripts/mowbot_camera_node.py
+++ scripts/mowbot_camera_node.py
@@ -11,12 +11,27 @@
     '''
 
 import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
 class MowbotCameraNode:
     myVar = 0
 
     def __init__(self):
         myVar = 10
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        cv2.imshow("Image", cv_image)
+        cv2.waitKey(1)
+
 
 
 
@@ -24,3 +39,9 @@ if __name__ == "__main__":
     rospy.init_node("mowbot_camera")
     node = MowbotCameraNode()
     rospy.spin()
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
